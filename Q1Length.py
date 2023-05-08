da90bd7d780aacc2ea7298a50b2415925e3ec6ec
2Brenny2 <124377719+2Brenny2@users.noreply.github.com>
2023-05-08 19:33:37 +0000

    Ooga Booga

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,7 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
+
+str = input("String: ")
+while str.lower() != "quit":
+    print(f"Length: {len(str)}")
+    str = input("String: ")
--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,4 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+
+str = input("String: ")
+for i in range(len(str)): print(str[::-1][i])
